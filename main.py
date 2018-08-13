422bb4fa7119ccebcfba7baba754b43f18189a3a
Moroo Jun <jun@shieldmail.net>
2018-08-13 21:27:41 +0900

    snap

--- main.py
+++ main.py
@@ -1,11 +1,112 @@
+from qrcode import constants, exceptions, util
+from qrcode.image.base import BaseImage
+import six
 import qrcode
 from PIL import Image
 import sys
 import copy
 
-def genpixmap(pixsize):
+class imgqrcode(qrcode.QRCode):
+    def makeImpl(self, test, mask_pattern):
+        #qrcode._check_version(self.version)
+        self.modules_count = self.version * 4 + 17
+        self.modules = [None] * self.modules_count
+
+        for row in range(self.modules_count):
+
+            self.modules[row] = [None] * self.modules_count
+
+            for col in range(self.modules_count):
+                self.modules[row][col] = None   # (col + row) % 3
+
+        self.setup_position_probe_pattern(0, 0)
+        self.setup_position_probe_pattern(self.modules_count - 7, 0)
+        self.setup_position_probe_pattern(0, self.modules_count - 7)
+        self.setup_position_adjust_pattern()
+        self.setup_timing_pattern()
+        self.keepmodules = copy.deepcopy(self.modules)
+        self.setup_type_info(test, mask_pattern)
+
+        if self.version >= 7:
+            self.setup_type_number(test)
+
+        if self.data_cache is None:
+            self.data_cache = util.create_data(
+                self.version, self.error_correction, self.data_list)
+        self.keepdata_cache = copy.deepcopy(self.data_cache)
+        self.map_data(self.data_cache, mask_pattern)
+
+    def adjustlevel(self, data):
+        """Make from map_data"""
+        inc = -1
+        row = self.modules_count - 1
+        bitIndex = 7
+        byteIndex = 0
+        self.maptest = []
+        for col in range(self.modules_count):
+            self.maptest.append([".",] * self.modules_count)
+
+        #mask_func = util.mask_func(mask_pattern)
+
+        data_len = len(data)
+        sumvalue=0
+
+        for col in six.moves.xrange(self.modules_count - 1, 0, -2):
+
+            if col <= 6:
+                col -= 1
+
+            col_range = (col, col-1)
+
+            while True:
+
+                for c in col_range:
+
+                    if self.keepmodules[row][c] is None:
+
+                        #dark = False
+
+                        #if byteIndex < data_len:
+                        #    dark = (((data[byteIndex] >> bitIndex) & 1) == 1)
+
+                        #if mask_func(row, c):
+                        #    dark = not dark
+
+                        #if byteIndex == 130:
+                        #    xp = True
+                        #else:
+                        #    xp = False
+                        if self.modules[row][c]:
+                            sumvalue += 1
+                        if bitIndex == 0:
+                            self.maptest[row][c] = "%d" % sumvalue
+                            sumvalue = 0
+                        
+                        bitIndex -= 1
+
+                        if bitIndex == -1:
+                            byteIndex += 1
+                            bitIndex = 7
+
+                row += inc
+
+                if row < 0 or self.modules_count <= row:
+                    row -= inc
+                    inc = -inc
+                    break
+
+    def targetimage(self,image,controlwidth=11):
+        codesize=self.modules_count
+        mp=genpixmap(8)
+        pic = Image.open(image)
+        picb = pic.convert("L")
+        ssize = int((codesize-controlwidth*2)/2)
+        pics = picb.resize((ssize,ssize))
+        self.picm = pics.point(mp)
+
+def genpixmap(targetlevel):
     """Generate pixel value map."""
-    v=256.0/(pixsize*pixsize+1)
+    v=256.0/(targetlevel+1)
     m=[]
     j=0
     for i in range(256):
@@ -45,11 +146,37 @@ def qrmap(qr,picm,controlwidth,pixsize):
     return samecount,nearcount,qr
 
 def make(code,image):
+    border=5
+    version=10
+    qr = imgqrcode(
+        version=version,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=1,
+        border=0,
+        mask_pattern=2)
+    qr.makeImpl(False,2)
+    qr.add_data(code)
+    qr.targetimage(image,controlwidth=11)
+    qr.adjustlevel(qr.keepdata_cache)
+    for n in qr.maptest:
+        t=""
+        for m in n:
+            t += m
+            #if m:
+            #    #t+="O"
+            #else:
+            #    #t+="."
+        print(t)
+                
+    img = qr.make_image()
+    img.show()
+
+def xmake(code,image):
     controlwidth=11 
     pixsize=2
     border=5
     version=10
-    dummyqr = qrcode.make(code,
+    dummyqr = imgqrcode.make(code,
         version=version,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
         box_size=1,
